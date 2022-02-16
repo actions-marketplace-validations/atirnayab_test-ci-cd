81fea8c799cdc34174be330018d1dd8e98e4781f
atir <nayabatir1@gmail.com>
2022-02-16 11:23:58 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const core = require("@actions/core");
+// const core = require("@actions/core");
 const { randomUUID } = require("crypto");
 
 console.log("inside");
 console.log(randomUUID());
-const value = core.getInput("token");
+// const value = core.getInput("token");
 
-console.log(value, 3524);
+// console.log(value, 3524);
