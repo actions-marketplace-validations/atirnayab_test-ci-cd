b4de6686e3803fbe029a37f2f613f74f7d8f65ed
atir nayab <nayabatir1@gmail.com>
2022-04-12 16:57:25 +0530

    detect file change

--- dist/index.js
+++ dist/index.js
@@ -17779,8 +17779,6 @@ const secret = core.getInput("SECRET");
 const file = readFileSync(path);
 
 async function testing() {
-  console.log(context.payload);
-
   try {
     if (!path) throw new Error("add doc path in workflow file");
     if (!projectKey) throw new Error("add DOCUMENT_KEY in github secret");
@@ -17789,6 +17787,32 @@ async function testing() {
     await ymlLint.lintFile(path).catch((err) => {
       throw new Error(err);
     });
+    let head, base;
+    const client = new GitHub(core.getInput("token", { required: true }));
+    const eventName = context.eventName;
+    switch (eventName) {
+      case "pull_request":
+        base = context.payload.pull_request?.base?.sha;
+        head = context.payload.pull_request?.head?.sha;
+        break;
+      case "push":
+        base = context.payload.before;
+        head = context.payload.after;
+        break;
+      default:
+        core.setFailed(
+          `This action only supports pull requests and pushes, ${context.eventName} events are not supported. ` +
+            "Please submit an issue on this action's GitHub repo if you believe this in correct."
+        );
+    }
+    const response = await client.repos.compareCommits({
+      base,
+      head,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    });
+
+    console.log(response);
 
     const type = path.split(".")[1];
     let config = {
@@ -17797,11 +17821,9 @@ async function testing() {
       },
     };
 
-    const path = "https://2d01-103-252-164-4.ngrok.io/github/update-doc";
-    console.log(path);
     const { data } = await axios
       .post(
-        path,
+        "https://2d01-103-252-164-4.ngrok.io/github/update-doc",
         {
           key: projectKey,
           file,
