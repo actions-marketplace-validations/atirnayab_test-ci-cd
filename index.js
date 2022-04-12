b66e91b0344cd75371204d8243a3547afbb4db29
atir nayab <nayabatir1@gmail.com>
2022-04-12 19:30:46 +0530

    detect file change

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function testing() {
     const { data: pullRequest } = await octokit.rest.pulls.get({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      pull_number: 123,
+      pull_number: context.payload.pull_request.number,
       mediaType: {
         format: "diff",
       },
